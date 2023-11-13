fafe4aff4e03438f9dbbcb98d39a544ea1eccc27
saiful-islam <md2215566@gmail.com>
2023-11-13 19:03:25 +0600

    job post deleted successfully

--- src/components/PostedCard/PostedCard.jsx
+++ src/components/PostedCard/PostedCard.jsx
@@ -2,9 +2,40 @@
 import { BsPenFill } from "react-icons/bs"
 import { RiDeleteBin6Line } from "react-icons/ri"
 import { Link } from "react-router-dom";
+import UseApi from "../../Hooks/UseApi";
+import Swal from "sweetalert2";
+
 
 const PostedCard = ({ data }) => {
    const { _id, title, date, category } = data;
+   const axios = UseApi();
+
+   const handleDelete = id => {
+      Swal.fire({
+         title: "Are you sure?",
+         text: "You won't be able to revert this!",
+         icon: "warning",
+         showCancelButton: true,
+         confirmButtonColor: "#3085d6",
+         cancelButtonColor: "#d33",
+         confirmButtonText: "Yes, delete it!"
+      })
+         .then((result) => {
+            if (result.isConfirmed) {
+               axios.delete(`/jobs/delete/${id}`)
+                  .then(res => {
+                     if (res.data.deletedCount > 0) {
+                        Swal.fire({
+                           title: "Deleted!",
+                           text: "You Job Post is Deleted!",
+                           icon: "success"
+                        });
+                     }
+                  })
+            }
+         });
+   }
+
    return (
       <div className="flex border p-6 rounded-xl shadow-lg justify-between">
          <div className=" space-y-2">
@@ -14,7 +45,7 @@ const PostedCard = ({ data }) => {
          </div>
          <div>
             <Link to={`/Posted-job/update/${_id}`}><BsPenFill className="text-2xl mb-12 text-success" /></Link>
-            <RiDeleteBin6Line className="text-2xl text-error" />
+            <RiDeleteBin6Line className="text-2xl text-error" onClick={() => handleDelete(_id)} />
          </div>
       </div>
    );
